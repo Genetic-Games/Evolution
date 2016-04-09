ea27460cb5444ecfe3fc18fb9716cb86180bc43a
Ian Santagata <7070901+iansantagata@users.noreply.github.com>
2016-04-09 17:58:15 -0400

    Evolution: updated game boundaries, destroys objects that manage to leave the boundary by high speed/force or sneaking by the boundary in some way

---
+++ Evolution/Assets/Scripts/DestroyByBoundary.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByBoundary : MonoBehaviour
+{
+	public GameController gameController;
+
+	public bool debug = true;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void OnTriggerExit2D (Collider2D other)
+	{
+		if (debug)
+			Debug.Log ("Object Leaving Boundary" + "\n" + other.gameObject + "\n" + "At Position: " + other.gameObject.transform.position);
+
+		GameObject gobj = other.gameObject;
+		if (gobj.tag == "Enemy") {
+			gameController.EnemyDestroyed ();
+			Destroy (gobj);
+		} else {
+			gameController.GameOver ();
+			gobj.SetActive (false);
+		}
+	}
+
+}
